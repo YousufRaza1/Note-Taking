d97f67f03ab225a515bd08240a749067723f5611
YousufRaza1 <yousuf.ice17@gmail.com>
2023-12-19 01:47:49 +0600

    WIP:Note details screen added

---
+++ Note-Taking/Views/NoteDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  NoteDetailsView.swift
+//  Note-Taking
+//
+//  Created by Yousuf on 12/19/23.
+//
+
+import SwiftUI
+
+struct NoteDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NoteDetailsView()
+}
