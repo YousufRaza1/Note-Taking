e1a83fa4898a83276fd7972ac97b8d6c765d9b22
YousufRaza1 <yousuf.ice17@gmail.com>
2023-12-18 23:28:09 +0600

    initial commit

---
+++ Note-Taking/Note_TakingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Note_TakingApp.swift
+//  Note-Taking
+//
+//  Created by Yousuf on 12/18/23.
+//
+
+import SwiftUI
+
+@main
+struct Note_TakingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
